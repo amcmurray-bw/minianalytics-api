a09856c7435c05aed0f269f2192e115c15456839
Aimee McMurray <amcmurray@brandwatch.com>
2018-08-13 11:40:56 +0200

    - moved Mention to DTO method to presenter, tidied up controller

--- src/main/java/amcmurray/bw/MentionPresenter.java
+++ src/main/java/amcmurray/bw/MentionPresenter.java
@@ -4,6 +4,8 @@ import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Component;
 
@@ -16,7 +18,7 @@ public class MentionPresenter {
     private static final DateTimeFormatter dateFormat
             = DateTimeFormatter.ISO_ZONED_DATE_TIME;
 
-    public MentionDTO toDTO(Mention mention) {
+    private MentionDTO toDTO(Mention mention) {
         String date = convertDate(mention.getCreatedAt());
 
         MentionDTO mentionDTO = new MentionDTO(mention.getId(),
@@ -32,5 +34,13 @@ public class MentionPresenter {
 
         //convert date to zoned date time, and then format to string
         return ZonedDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault()).format(dateFormat);
+    }
+
+    //takes list of mentions, converts them to MentionDTO and returns
+    public List<MentionDTO> toDTOs(List<Mention> allMentions) {
+        return allMentions
+                .stream()
+                .map(this::toDTO)
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/amcmurray/bw/controllers/MentionsController.java
+++ src/main/java/amcmurray/bw/controllers/MentionsController.java
@@ -1,6 +1,5 @@
 package amcmurray.bw.controllers;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import amcmurray.bw.MentionPresenter;
 import amcmurray.bw.services.MentionService;
-import amcmurray.bw.twitterdomainobjects.Mention;
 import amcmurray.bw.twitterdomainobjects.MentionDTO;
 
 @RestController
@@ -28,23 +26,14 @@ public class MentionsController {
     //view mentions of single query
     @GetMapping("/mentions/{id}")
     public List<MentionDTO> viewMentionsOfQuery(@PathVariable("id") int id) {
-        return convertMentionsToMentionDTO(mentionService.findAllMentionsOfQuery(id));
+        return mentionPresenter.toDTOs(
+                mentionService.findAllMentionsOfQuery(id));
     }
 
     //view all mentions
     @GetMapping("/mentions")
     public List<MentionDTO> viewAllMentions() {
-        return convertMentionsToMentionDTO(mentionService.getAllMentions());
+        return mentionPresenter.toDTOs(
+                mentionService.getAllMentions());
     }
-
-    //takes list of mentions, converts them to MentionDTO and returns
-    private List<MentionDTO> convertMentionsToMentionDTO(List<Mention> allMentions) {
-        List<MentionDTO> listOfMentionDTO = new ArrayList<>();
-
-        for (Mention mention : allMentions) {
-            listOfMentionDTO.add(mentionPresenter.toDTO(mention));
-        }
-        return listOfMentionDTO;
-    }
-
 }
